dd8d5162b5ab8ba158415773bdc7bec0f9ed4ff3
christine <gacuirichristine@gmail.com>
2020-05-13 12:33:08 +0300

    Initial commit.

---
+++ src/app/workout-runner/workout-runner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abe-workout-runner',
+  templateUrl: './workout-runner.component.html',
+  styleUrls: []
+})
+export class WorkoutRunnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
