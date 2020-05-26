19dbea094492111e00c5f569e7741953f817f484
christine <gacuirichristine@gmail.com>
2020-05-26 23:53:35 +0300

    Number our steps in unordered list.

--- src/app/workout-runner/workout-runner.component.ts
+++ src/app/workout-runner/workout-runner.component.ts
@@ -153,8 +153,8 @@ workout.exercises.push(
       'A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.',
       'wallsit.png',
       'wallsit.wav',
-      `Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.
-      Then, keeping your back against the wall, lower your hips until your knees form right angles.`,
+      `<ol><li>Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.</li>
+      <li>Then, keeping your back against the wall, lower your hips until your knees form right angles.</li></ol>`,
       ['y-wV4Venusw', 'MMV3v4ap4ro']),
     30));
 
@@ -166,9 +166,9 @@ workout.exercises.push(
       'A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms',
       'Pushup.png',
       'pushups.wav',
-      `Lie prone on the ground with hands placed as wide or slightly wider than shoulder width.
-      Keeping the body straight, lower body to the ground by bending arms at the elbows.
-      Raise body up off the ground by extending the arms.`,
+      `<ol><li>Lie prone on the ground with hands placed as wide or slightly wider than shoulder width.</li>
+     <li> Keeping the body straight, lower body to the ground by bending arms at the elbows.</li>
+      <li>Raise body up off the ground by extending the arms.</li></ol>`,
       ['Eh00_rniF8E', 'ZWdBqFLNljc', 'UwRLWMcOdwI', 'ynPwl6qyUNM', 'OicNTT2xzMI']),
     30));
 
@@ -180,12 +180,12 @@ workout.exercises.push(
       'The basic crunch is a abdominal exercise in a strength-training program.',
       'crunches.png',
       'crunches.wav',
-      `Lie on your back with your knees bent and feet flat on the floor, hip-width apart.
-      Place your hands behind your head so your thumbs are behind your ears.
-      Hold your elbows out to the sides but rounded slightly in.
-      Gently pull your abdominals inward.
-      Curl up and forward so that your head, neck, and shoulder blades lift off the floor.
-      Hold for a moment at the top of the movement and then lower slowly back down.`,
+      `<ol><li>Lie on your back with your knees bent and feet flat on the floor, hip-width apart.</li>
+      <li>Place your hands behind your head so your thumbs are behind your ears.</li>
+      <li>Hold your elbows out to the sides but rounded slightly in.</li>
+     <li> Gently pull your abdominals inward.</li>
+      <li>Curl up and forward so that your head, neck, and shoulder blades lift off the floor.</li>
+      <li>Hold for a moment at the top of the movement and then lower slowly back down.</li></ol>`,
       ['Xyd_fa5zoEU', 'MKmrqcoCZ-M']),
     30));
 
@@ -197,10 +197,10 @@ workout.exercises.push(
       'Step exercises are ideal for building muscle in your lower body.',
       'stepUpOntoChair.png',
       'stepup.wav',
-      `Position your chair in front of you.
-      Stand with your feet about hip width apart, arms at your sides.
-      Step up onto the seat with one foot, pressing down while bringing your other foot up next to it.
-      Step back with the leading foot and bring the trailing foot down to finish one step-up.`,
+      `<ol><li>Position your chair in front of you.</li>
+     <li> Stand with your feet about hip width apart, arms at your sides.</li>
+     <li> Step up onto the seat with one foot, pressing down while bringing your other foot up next to it.</li>
+     <li> Step back with the leading foot and bring the trailing foot down to finish one step-up.</li></ol>`,
       ['aajhW7DD1EA']),
     30));
 
@@ -212,12 +212,12 @@ workout.exercises.push(
       'The squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips, buttocks and quads.',
       'squat.png',
       'squats.wav',
-      `Stand with your head facing forward and your chest held up and out.
-          Place your feet shoulder-width apart or little wider. Extend your hands straight out in front of you.
-          Sit back and down like you're sitting into a chair. Keep your head facing straight as your upper body bends forward a bit.
-          Rather than allowing your back to round, let your lower back arch slightly as you go down.
-          Lower down so your thighs are parallel to the floor, with your knees over your ankles. Press your weight back into your heels.
-          Keep your body tight, and push through your heels to bring yourself back to the starting position.`,
+      `<ol><li>Stand with your head facing forward and your chest held up and out.</li>
+         <li> Place your feet shoulder-width apart or little wider. Extend your hands straight out in front of you.</li>
+         <li> Sit back and down like you're sitting into a chair. Keep your head facing straight as your upper body bends forward a bit.</li>
+         <li> Rather than allowing your back to round, let your lower back arch slightly as you go down.</li>
+         <li> Lower down so your thighs are parallel to the floor, with your knees over your ankles. Press your weight back into your heels.</li>
+         <li> Keep your body tight, and push through your heels to bring yourself back to the starting position.</li></ol>`,
       ['QKKZ9AGYTi4', 'UXJrBgI2RxA']),
     30));
 
@@ -229,10 +229,10 @@ workout.exercises.push(
       'A body weight exercise that targets triceps.',
       'tricepdips.png',
       'tricepdips.wav',
-      `Sit up on a chair. Your legs should be slightly extended, with your feet flat on the floor.
-      Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.
-      Without moving your legs, bring your glutes forward off the chair.
-      Steadily lower yourself. When your elbows form 90 degrees angles, push yourself back up to starting position.`,
+      `<ol><li>Sit up on a chair. Your legs should be slightly extended, with your feet flat on the floor.</li>
+     <li> Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.</li>
+     <li> Without moving your legs, bring your glutes forward off the chair.</li>
+     <li> Steadily lower yourself. When your elbows form 90 degrees angles, push yourself back up to starting position.</li></ol>`,
       ['tKjcgfu44sI', 'jox1rb5krQI']),
     30));
 
@@ -245,10 +245,10 @@ workout.exercises.push(
       involves maintaining a difficult position for extended periods of time.`,
       'Plank.png',
       'plank.wav',
-      `Get into pushup position on the floor.
-      Bend your elbows 90 degrees and rest your weight on your forearms.
-      Your elbows should be directly beneath your shoulders, and your body should form a straight line from head to feet.
-      Hold this position.`,
+      `<ol><li>Get into pushup position on the floor.</li>
+      <li>Bend your elbows 90 degrees and rest your weight on your forearms.</li>
+     <li> Your elbows should be directly beneath your shoulders, and your body should form a straight line from head to feet.</li>
+     <li> Hold this position.</li></ol>`,
       ['pSHjTRCQxIw', 'TvxNkmjdhMM']),
     30));
 
@@ -260,8 +260,8 @@ workout.exercises.push(
       'A form exercise that develops strength and endurance of the hip flexors and quads and stretches the hip extensors.',
       'highknees.png',
       'highknees.wav',
-      `Start standing with feet hip-width apart.
-      Do inplace jog with your knees lifting as much as possible towards your chest.`,
+      `<ol><li>Start standing with feet hip-width apart.</li>
+      <li>Do inplace jog with your knees lifting as much as possible towards your chest.</li></ol>`,
       ['OAJ_J3EZkdY', '8opcQdC-V-U']),
     30));
 
@@ -274,8 +274,8 @@ workout.exercises.push(
       including the quadriceps (or thighs), the gluteus maximus (or buttocks) as well as the hamstrings.`,
       'lunges.png',
       'lunge.wav',
-      `Start standing with feet hip-width apart.
-          Do inplace jog with your knees lifting as much as possible towards your chest.`,
+      `<ol><li>Start standing with feet hip-width apart.</li>
+         <li> Do inplace jog with your knees lifting as much as possible towards your chest.</li></ol>`,
       ['Z2n58m2i4jg']),
     30));
 
@@ -287,8 +287,8 @@ workout.exercises.push(
       'A variation of pushup that requires you to rotate.',
       'pushupNRotate.png',
       'pushupandrotate.wav',
-      `Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.
-      Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling.`,
+      `<ol></li>Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.</li>
+     <li>Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling.</li></ol>`,
       ['qHQ_E-f5278']),
     30));
 
@@ -300,10 +300,10 @@ workout.exercises.push(
       'A variation to Plank done using one hand only.',
       'sideplank.png',
       'sideplank.wav',
-      `Lie on your side, in a straight line from head to feet, resting on your forearm.
-      Your elbow should be directly under your shoulder.
-      With your abdominals gently contracted, lift your hips off the floor, maintaining the line.
-      Keep your hips square and your neck in line with your spine. Hold the position.`,
+      `<ol><li>Lie on your side, in a straight line from head to feet, resting on your forearm.</li>
+      <li>Your elbow should be directly under your shoulder.</li>
+     <li> With your abdominals gently contracted, lift your hips off the floor, maintaining the line.</li>
+     <li> Keep your hips square and your neck in line with your spine. Hold the position.</li></ol>`,
       ['wqzrb67Dwf8', '_rdfjFSFKMY']),
     30));
   
