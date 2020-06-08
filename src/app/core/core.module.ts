5c2597abc9185a931481e9ab35986cabedffc8fb
Christine <teenahshyro@gmail.com>
2020-06-08 15:27:00 +0300

    Pushing to github.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { WorkoutHistoryTrackerService } from './workout-history-tracker.service';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers:[
+    WorkoutHistoryTrackerService
   ]
 })
 export class CoreModule { }
