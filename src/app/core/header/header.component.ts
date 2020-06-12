ca6cef27e64bf1220d68fbb8d77131a8eb2c5b85
christine <gacuirichristine@gmail.com>
2020-06-12 12:57:18 +0300

    Add bootstrap navbar.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'abe-header',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent implements OnInit {
+ showHistoryLink:boolean;
+
+  constructor(private router:Router) { 
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.showHistoryLink = !e.url.startsWith('/workout');
+      })
+  }
 
   ngOnInit(): void {
   }
