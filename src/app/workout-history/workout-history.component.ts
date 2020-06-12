60b93daa9db2912e9c04508abb9980581ba13ab5
christine <gacuirichristine@gmail.com>
2020-06-12 13:03:46 +0300

    Initial commit.

---
+++ src/app/workout-history/workout-history.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abe-workout-history',
+  templateUrl: './workout-history.component.html',
+  styles: [
+  ]
+})
+export class WorkoutHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
